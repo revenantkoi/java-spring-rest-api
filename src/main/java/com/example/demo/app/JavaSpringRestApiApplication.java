bbad5c5634e631f217f5645727be6b64109ec15a
revenantkoi <rippvine@gmail.com>
2019-04-13 09:15:45 -0400

    enable entity, component, and repository scanning in main

--- src/main/java/com/example/demo/app/JavaSpringRestApiApplication.java
+++ src/main/java/com/example/demo/app/JavaSpringRestApiApplication.java
@@ -2,8 +2,14 @@ package com.example.demo.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan("com.example.demo")
+@EntityScan("com.example.demo.model")
+@EnableJpaRepositories("com.example.demo.repository")
 public class JavaSpringRestApiApplication {
 
 	public static void main(String[] args) {
