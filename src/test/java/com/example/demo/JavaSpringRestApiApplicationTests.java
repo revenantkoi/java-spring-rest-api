11b360725b55a6edf47f000066cec471e4643564
revenantkoi <rippvine@gmail.com>
2019-04-12 22:52:35 -0400

    modify directory structure; fix broken test

--- src/test/java/com/example/demo/JavaSpringRestApiApplicationTests.java
+++ src/test/java/com/example/demo/JavaSpringRestApiApplicationTests.java
@@ -1,12 +1,13 @@
 package com.example.demo;
 
+import com.example.demo.app.JavaSpringRestApiApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = JavaSpringRestApiApplication.class)
 public class JavaSpringRestApiApplicationTests {
 
 	@Test
