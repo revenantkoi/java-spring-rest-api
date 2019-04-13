949f05cea708f8eeaf2ad35267551ffb56641557
revenantkoi <rippvine@gmail.com>
2019-04-13 09:18:42 -0400

    CommandLineRunner with initial Person data for testing

--- src/main/java/com/example/demo/app/JavaSpringRestApiApplication.java
+++ src/main/java/com/example/demo/app/JavaSpringRestApiApplication.java
@@ -1,10 +1,18 @@
 package com.example.demo.app;
 
+import com.example.demo.model.Person;
+import com.example.demo.repository.PersonRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 @SpringBootApplication
 @ComponentScan("com.example.demo")
@@ -12,8 +20,16 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories("com.example.demo.repository")
 public class JavaSpringRestApiApplication {
 
-	public static void main(String[] args) {
+    private Date date = new GregorianCalendar(1910, Calendar.FEBRUARY, 11).getTime();
+
+    public static void main(String[] args) {
 		SpringApplication.run(JavaSpringRestApiApplication.class, args);
 	}
 
+    @Bean
+    CommandLineRunner commandLineRunner(PersonRepository personRepository) {
+        return args -> {
+            personRepository.save(new Person("Deepak", date,"NonLocality Street",5, 200, 45));
+        };
+    }
 }
