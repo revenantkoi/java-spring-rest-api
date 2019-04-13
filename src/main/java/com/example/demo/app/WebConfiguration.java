f9040c589ad0fe17691ca1708d970ec053dce4e5
revenantkoi <rippvine@gmail.com>
2019-04-13 09:13:27 -0400

    configure h2 in application.properties

--- src/main/java/com/example/demo/app/WebConfiguration.java
+++ src/main/java/com/example/demo/app/WebConfiguration.java
@@ -8,8 +8,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class WebConfiguration {
     @Bean
-    ServletRegistrationBean h2servletRegistration(){
-        ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
+    ServletRegistrationBean<WebServlet> h2servletRegistration() {
+        ServletRegistrationBean<WebServlet> registrationBean = new ServletRegistrationBean<>(new WebServlet());
         registrationBean.addUrlMappings("/console/*");
         return registrationBean;
     }
